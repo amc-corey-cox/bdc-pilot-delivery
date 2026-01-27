ed8840307377c07911501b2b7eae694e134985e4
amc-corey-cox <69321580+amc-corey-cox@users.noreply.github.com>
2026-01-27 12:00:04 -0600

    Add config files for 12 outstanding consent groups (closes #3)

---
+++ studies/MESA/config-c2.mk
@@ -0,0 +1,15 @@
+# MESA - Multi-Ethnic Study of Atherosclerosis
+# dbGaP: phs000209
+# Version: v13, Consent: c2 (HMB-NPU)
+#
+# 6,814 participants aged 45-84, free of clinical CVD at baseline.
+# Ethnic distribution: 38% White, 28% African-American, 22% Hispanic, 12% Asian
+# Six field centers across the US
+
+DM_INPUT_DIR         := /sbgenomics/workspace/output/MESA_c2
+DM_SCHEMA_NAME       := MESA_v13_c2
+DM_OUTPUT_DIR        := /sbgenomics/workspace/output/MESA/Schema_MESA_v13_c2
+DM_TRANS_SPEC_DIR    := /sbgenomics/workspace/NHLBI-BDC-DMC-HV/priority_variables_transform/MESA-ingest
+DM_MAP_TARGET_SCHEMA := /sbgenomics/project-files/bdchm.yaml
+DM_MAPPING_PREFIX    := MESA
+DM_MAPPING_POSTFIX   := -HMB-NPU-data
